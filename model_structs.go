45622bc0ec421e3df6ba40cf04e05332b5d7d0f1
dannetrichard <691072014@qq.com>
2021-02-28 17:23:59 +0800

    id 加json

--- model_structs.go
+++ model_structs.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Model struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
 
 type User struct {
