a91817cbd43337b605eb08a5f9c78e7d58f1cb99
dannetrichard <691072014@qq.com>
2021-03-05 21:06:08 +0800

    修改db

--- model_api.go
+++ model_api.go
@@ -5,30 +5,29 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-var DB *gorm.DB
-
-func Init() {
-	DB, _ = gorm.Open("mysql", "root:Jinjun123!@tcp(49.234.129.151:3306)/coupon?charset=utf8&parseTime=True&loc=Local")
-}
-
-func Close() {
-	DB.Close()
+func Init() *gorm.DB {
+	db, _ := gorm.Open("mysql", "root:Jinjun123!@tcp(49.234.129.151:3306)/coupon?charset=utf8&parseTime=True&loc=Local")
+	db.LogMode(true)               //打印sql语句
+	db.DB().SetMaxIdleConns(100)   //最大空闲连接
+	db.DB().SetMaxOpenConns(10000) //最大连接数
+	db.DB().SetConnMaxLifetime(30) //最大生存时间(s)
+	return db
 }
 
 func Renew() {
-	Init()
-	defer Close()
-	DB.Set("gorm:table_options", "ENGINE=InnoDB")
+	db := Init()
+	defer db.Close()
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
 
-	DB.DropTableIfExists(&User{}, &Refund{}, &Ship{}, &Cat{}, &Item{}, &Trade{}, &Order{})
-	DB.CreateTable(&User{}, &Refund{}, &Ship{}, &Cat{}, &Item{}, &Trade{}, &Order{})
-	DB.Create(&Refund{RefundID: "101502290600487397", Status: "NEWHERE"})
-	DB.Create(&User{Openid: "ocpij4ivTmwb1o_IrWbp912Y4cRE", WeChat: "fg2060", BuyerNick: "印霜设计"})
+	db.DropTableIfExists(&User{}, &Refund{}, &Ship{}, &Cat{}, &Item{}, &Trade{}, &Order{})
+	db.CreateTable(&User{}, &Refund{}, &Ship{}, &Cat{}, &Item{}, &Trade{}, &Order{})
+	db.Create(&Refund{RefundID: "101502290600487397", Status: "NEWHERE"})
+	db.Create(&User{Openid: "ocpij4ivTmwb1o_IrWbp912Y4cRE", WeChat: "fg2060", BuyerNick: "印霜设计"})
 }
 
 func Migrate() {
-	Init()
-	defer Close()
-	DB.Set("gorm:table_options", "ENGINE=InnoDB")
-	DB.AutoMigrate(&User{}, &Refund{}, &Ship{}, &Cat{}, &Item{}, &Trade{}, &Order{})
+	db := Init()
+	defer db.Close()
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
+	db.AutoMigrate(&User{}, &Refund{}, &Ship{}, &Cat{}, &Item{}, &Trade{}, &Order{})
 }
